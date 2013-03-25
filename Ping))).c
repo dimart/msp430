7da72f8913be762ac1fd9b19fec03dd01301a510
Дмитрий Петухов <dimart.sp@gmail.com>
2013-03-25 23:19:59 +0400

    Create Ping))).c

---
+++ Ping))).c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <msp430.h> 
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;
+
+	P1DIR |= BIT4;
+	P1OUT |= BIT4;
+
+  //<magic>
+	//UCSCTL0 = 0x0000;
+	//UCSCTL0 = 0x0000;                         // Set lowest possible DCOx, MODx
+	  UCSCTL1 = DCORSEL_5;                      // Select DCO range 24MHz operation
+	  UCSCTL2 = 1;
+
+	  UCSCTL3 |= SELREF_2;                      // Set DCO FLL reference = REFO
+	  UCSCTL4 |= SELS_2;
+	  UCSCTL6 |= XT2DRIVE_2;
+	//</magic>
+
+	TA0CCR0 = 1;
+	TA0CTL = TASSEL_2 + MC_1 + TACLR  ;
+	TA0CCTL0 = CCIE;
+
+
+	while (1)
+	{
+		__bis_SR_register(LPM0_bits + GIE);
+		__no_operation();
+	}
+	return 0;
+}
+
+// Timer0 A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void)
+{
+  P1OUT = 0x0;                            // Toggle P1.0
+}
+
